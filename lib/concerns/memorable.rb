a8549b2d41b376006f0141e89a068838e02d1ca8
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-20 23:24:33 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,13 @@
 module Memorable
 
-  def reset_all
-   all.clear
-  end
+  module ClassMethods
+    
+    def reset_all
+     all.clear
+    end
 
-  def count
-    all.count
-  end
+    def count
+      all.count
+    end
 
 end
